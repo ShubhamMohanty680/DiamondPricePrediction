470cc39c209c1382a9e4a599c8bec8910791e925
ShubhamMohanty680 <shubahamohanty680@gmail.com>
2023-07-30 19:18:27 +0530

    update project

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Krish',
-    author_email='krish.naik@pw.live',
+    author='Shubham Mohanty',
+    author_email='shubham.crusader@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
